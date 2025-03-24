10acaa443193462cd1f3b7b44adca13eeda6e8c0
ProfessionalMenace <vondracekadam00@gmail.com>
2025-03-24 20:55:38 +0100

    Refactoring

--- src/interpreter.hpp
+++ src/interpreter.hpp
@@ -4,16 +4,13 @@
 struct Interpreter {
     std::vector<char> memory_;
     size_t memory_idx;
-    size_t token_idx;
     bool stop_flag;
 
-    Interpreter() : memory_(), memory_idx(), token_idx(), stop_flag() {
-        memory_.push_back(0);
-    }
+    Interpreter() : memory_(100, 0), memory_idx(), stop_flag() {}
 
     /* outputs state in a hex codes */ 
     void print_state(void) {
-        std::print("Memory[{}], Token[{}], Cells:", memory_idx, token_idx);
+        std::print("Memory[{}], Cells:", memory_idx);
         for(size_t i = 0; i < memory_.size(); ++i) {
             std::print("[{:02x}]", memory_[i]);
         }
@@ -33,7 +30,7 @@ struct Interpreter {
     }
 
     /* Increment the byte at the data pointer by value */
-    void increment(size_t value) {
+    void increment(size_t &value) {
         size_t tmp = memory_[memory_idx] + value;
         if(tmp >= 0xFF) {
             std::cerr << "Memory overflow\n";
@@ -44,7 +41,7 @@ struct Interpreter {
     }
 
     /* Decrement the byte at the data pointer by value. */
-    void decrement(size_t value) {
+    void decrement(size_t &value) {
         size_t tmp = memory_[memory_idx];
         if(tmp < value) {
             std::cerr << "Memory underflow\n";
@@ -55,7 +52,7 @@ struct Interpreter {
     }
 
     /* Increment the data pointer by value. */
-    void move_right(size_t value) {
+    void move_right(size_t &value) {
         memory_idx += value;
         while(memory_idx >= memory_.size()) {
             memory_.push_back(0);
@@ -63,7 +60,7 @@ struct Interpreter {
     }
 
     /* Decrement the data pointer by one */
-    void move_left(size_t value) {
+    void move_left(size_t &value) {
         if(value > memory_idx) {
             stop_flag = true;
             std::cerr << "Memory pointer underflow\n";
@@ -73,25 +70,16 @@ struct Interpreter {
     }
 
     /* Output the byte at the data pointer. */
-    void output(char value) {
+    void output(size_t &value) {
         while(value--) {
-            std::print("{}", memory_[memory_idx]);
+            std::print("{}", static_cast<char>(memory_[memory_idx]));
         }
     }
 
     /* Accept one byte of input, storing its value in the byte at the data pointer. */
-    void input(auto value) {
-        if(value) { std::cerr << "value ignored\n"; }
-        memory_[memory_idx] = std::cin.get();
-    }
-
-    /* Jump to a position inside of a token buffer */
-    void jump (std::vector<Tokenizer::Token> &tokens) {
-        if(token_idx < tokens.size()) {
-            token_idx = tokens[token_idx].data;
-        } else {
-            std::cerr << "Tried accessing value outside the token buffer\n";
-            stop_flag = true;
+    void input(size_t &value) {
+        while(value--) {
+            memory_[memory_idx] = std::cin.get();
         }
     }
 
@@ -100,8 +88,18 @@ struct Interpreter {
     }
 
     void interpret(std::vector<Tokenizer::Token> &tokens) {
-        token_idx = 0;
-        stop_flag = false;
+        size_t token_idx = 0;
+
+        /* Jump to a position inside of a token buffer */
+        auto jump = [this, &token_idx, &tokens](size_t &value) {
+            if(token_idx < tokens.size()) {
+                token_idx = value;
+            } else {
+                std::cerr << "Tried accessing value outside the token buffer\n";
+                stop_flag = true;
+            }
+        };
+
         while(token_idx < tokens.size() && !stop_flag) {
             auto [type, value] = tokens[token_idx];
             switch(type) {
@@ -113,12 +111,12 @@ struct Interpreter {
                 case Tokenizer::Symbol::in  : input(value);      break;
                 case Tokenizer::Symbol::loop_begin: 
                     if(memory_[memory_idx] == 0) {
-                        jump(tokens);
+                        jump(value);
                     }
                     break;
                 case Tokenizer::Symbol::loop_end: 
                     if(memory_[memory_idx] != 0) {
-                        jump(tokens);
+                        jump(value);
                     }
                     break;
                 case Tokenizer::Symbol::bad: [[fallthrough]];
